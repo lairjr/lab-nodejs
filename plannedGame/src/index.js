a9aeb89183516d5712ad9a7c50c1b8dd67ae9eed
Lair Junior <lairjr@outlook.com>
2016-07-23 17:04:26 -0300

    Change to express application

--- plannedGame/src/index.js
+++ plannedGame/src/index.js
@@ -1,8 +1,11 @@
-import http from 'http';
+import express from 'express';
 
-http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
+const app = express();
 
-console.log('Server running at http://127.0.0.1:1337/');
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
--- plannedGame/test/index_spec.js
+++ plannedGame/test/index_spec.js
@@ -3,9 +3,9 @@ import assert from 'assert';
 
 import '../src/index.js';
 
-describe('Example Node Server', () => {
-  it('should return 200', done => {
-    http.get('http://127.0.0.1:1337', res => {
+describe('index', () => {
+  it('returns 200', done => {
+    http.get('http://127.0.0.1:3000', res => {
       assert.equal(200, res.statusCode);
       done();
     });
